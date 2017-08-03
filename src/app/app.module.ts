4cc42e43d187325ca4ea8e9804c603179adb7cb1
IMUDGES-PJH <panjiahui@imudges.com>
2017-08-03 18:03:38 +0800

    LoveApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
+import { NotePage } from '../pages/note/note';
+import { PicturePage } from '../pages/picture/picture';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+import { SettingPage } from '../pages/setting/setting';
+import { MemoryPage } from '../pages/memory/memory';
+import { TreePage } from '../pages/tree/tree';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,10 +18,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    NotePage,
+    PicturePage,
+    MemoryPage,
+    TreePage,
+    TabsPage,
+    LoginPage,
+    SettingPage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +33,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    NotePage,
+    PicturePage,
+    MemoryPage,
+    TreePage,
+    TabsPage,
+    LoginPage,
+    SettingPage
   ],
   providers: [
     StatusBar,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,19 +1,44 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
-
+import { NotePage } from '../note/note';
+import { PicturePage } from '../picture/picture';
+import { SettingPage } from '../setting/setting';
+import { MemoryPage } from '../memory/memory';
+import { TreePage } from '../tree/tree';
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tabRoots: Object[];
+  constructor(){
+    this.tabRoots=[
+      {
+        root:NotePage,
+        tabTitle:'表白',
+        tabIcon:'heart'
+      },
+      {
+        root:PicturePage,
+        tabTitle:'相册',
+        tabIcon:'image'
+      },
+      {
+        root:MemoryPage,
+        tabTitle:'纪念日',
+        tabIcon:'rose',
+      },
+      {
+        root:TreePage,
+        tabTitle:'许愿树',
+        tabIcon:'leaf'
+      },
+      {
+        root:SettingPage,
+        tabTitle:'我的',
+        tabIcon:'pulse'
 
-  constructor() {
-
+      }
+    ];
   }
 }
